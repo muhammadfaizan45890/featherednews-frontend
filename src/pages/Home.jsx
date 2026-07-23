e95a162dc185435675006fd3ffb658cc1a21ee0c
Muhammad Faizan <muhammadfaizan45890@gmail.com>
2026-07-23 18:34:13 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Hero from '@/components/Hero'
 import HNews from '@/components/HNews'
 import LatestStories from '@/components/LatestStories'
 import React from 'react'
+import BlogCardSlider from './FeaturedStories'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <BlogCardSlider/>
       <LatestStories/>
       <HNews/>
       {/* <Footer/> */}
